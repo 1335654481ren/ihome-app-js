ac57c0fe2c4c95c16b6588f2a996d2b89ec22d4e
1335654481ren <1335654481@qq.com>
2025-01-22 12:27:53 +0800

    fix buf

--- index.js
+++ index.js
@@ -148,6 +148,10 @@ app.get("/", async (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
+app.get("/ota", async (req, res) => {
+  res.sendFile(path.join(__dirname, "otaupload.html"));
+});
+
 // 更新计数
 app.post("/api/count", async (req, res) => {
   const { action } = req.body;
